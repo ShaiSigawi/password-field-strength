ad5cffbb1c28b3633bd22cae44c4ff3cac250608
Shai <shaisigawi@gmail.com>
2022-12-13 13:21:25 +0200

    Pass-Field-Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import {React} from 'react';
+import Testing from './Testing';
 
 function App() {
+  const [password, setPassword] = useState('')
+  //const [numChar, setNumChar] = useState(0)
+
+  function handlePasswordChange(e){
+    console.log(e)
+    setPassword(e.target.value)
+  }
+
+  function handelPasswordLength(e){
+    if (password.length <= 2 || password.length > 0){
+       //console.log("weak")
+       setPassword('weak')
+    }
+    if (password.length > 2 || password.length <= 6){
+      setPassword('medium')
+  }
+    else{
+     setPassword('strong')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Password App Check</h1>
+      <input value={password} onChange={handlePasswordChange}/>
+      <button>Submit</button>
+      <form onSubmit={handelPasswordLength}></form>
+      <p>{setPassword}</p>
+
+      <Testing/>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/Testing.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {useState} from 'react';
+import {React} from 'react';
+
+function Testing() {
+
+
+  return (
+    <div className="Testing">
+      <p>Test</p>
+    </div>
+  );
+}
+
+export default Testing;
